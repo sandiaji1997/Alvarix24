f2cd82849eb69bc2852b497f1ee73f696b36bbfe
Sandiaji <emailkamu@gmail.com>
2026-05-28 02:34:17 +0700

    fix: normalize linux filenames

--- Alvarix/backend/routes/authroutes.js
+++ Alvarix/backend/routes/authroutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/User');
+const User = require('../models/user');
 
 // ================= REGISTER =================
 router.post('/register', async (req, res) => {
