8942a7d0fbb48c5ce16636ca61a275981b82d5e9
Sandiaji <emailkamu@gmail.com>
2026-05-27 22:40:40 +0700

    Fix JWT authentication final

--- Alvarix/backend/middleware/verifyToken.js
+++ Alvarix/backend/middleware/verifyToken.js
@@ -3,33 +3,27 @@ const jwt = require('jsonwebtoken')
 const verifyToken = (req, res, next) => {
   try {
 
-    // ambil authorization header
     const authHeader = req.headers.authorization
 
-    // cek apakah ada header
     if (!authHeader) {
       return res.status(401).json({
         error: 'Token required'
       })
     }
 
-    // format: Bearer token
-    const token = authHeader.split(' ')[1]
-
-    // cek token kosong
-    if (!token) {
+    if (!authHeader.startsWith('Bearer ')) {
       return res.status(401).json({
         error: 'Invalid token format'
       })
     }
 
-    // verify token
+    const token = authHeader.split(' ')[1]
+
     const decoded = jwt.verify(
       token,
       process.env.JWT_SECRET
     )
 
-    // simpan user data ke request
     req.user = decoded
 
     next()
--- Alvarix/backend/routes/authRoutes.js
+++ Alvarix/backend/routes/authRoutes.js
@@ -45,10 +45,14 @@ router.post('/login', async (req, res) => {
     }
 
     const token = jwt.sign(
-      { userId: user._id },
-      'SECRET_KEY',
-      { expiresIn: '1d' }
-    );
+   {
+    userId: user._id
+   },
+   process.env.JWT_SECRET,
+   {
+    expiresIn: '24h'
+   }
+ )
 
     res.json({
       message: 'Login success',
